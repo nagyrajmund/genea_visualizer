e3b59e714600fb7a69763f3bb9f81217a803ba23
nagyrajmund <rajmund977@gmail.com>
2025-04-29 02:35:19 +0200

    Update `check_files_npz` to use the reference SMPLX data from the test set for non-generated fields

--- celery-queue/scripts/load_data.py
+++ celery-queue/scripts/load_data.py
@@ -49,89 +49,31 @@ def check_files_npz(SMPLX_LOCATION, SMPLX_FILENAME, SMPLX_LOCATION_DATASET = Non
     SMPLX_TAKE = myPath(str(SMPLX_LOCATION) + '/' + str(SMPLX_FILENAME) + '.npz')
     smplx_loaded_data = np.load(SMPLX_TAKE, allow_pickle=True)
     
-    # SMPLX_TAKE_DATASET = myPath(str(SMPLX_LOCATION_DATASET) + '/' + str(SMPLX_FILENAME_DATASET) + '.npz')
+    SMPLX_TAKE_DATASET = myPath(str(SMPLX_LOCATION_DATASET) + '/' + str(SMPLX_FILENAME_DATASET) + '.npz')
     
-    # if SMPLX_FILENAME_DATASET is None:
-    #     print(f'Provided dataset file is None! ', str(SMPLX_FILENAME_DATASET))
-    #     SMPLX_FILENAME_DATASET = SMPLX_FILENAME
-    #     print(f'Attempt to find file with same name in dataset! ', {SMPLX_FILENAME_DATASET})
-    #     SMPLX_TAKE_DATASET = myPath(str(SMPLX_LOCATION_DATASET) + '/' + str(SMPLX_FILENAME_DATASET) + '.npz')
-    #     print(f'File from dataset! ', str(SMPLX_TAKE_DATASET))
     
-    # if not SMPLX_TAKE_DATASET.exists():
-    #     print(f'Provided file not found! Attempt string comparison from filename IF filename structure is similar to dataset filenaming!')
-    #     SMPLX_FILENAME_DATASET_clean = SMPLX_FILENAME_DATASET.rsplit('_sample_', 1)[0]
-    #     SMPLX_TAKE_DATASET = myPath(str(SMPLX_LOCATION_DATASET) + '/' + str(SMPLX_FILENAME_DATASET_clean) + '.npz')
+    smplx_dataset_data = np.load(SMPLX_TAKE_DATASET, allow_pickle=True)
     
-    # if not SMPLX_TAKE_DATASET.exists() or SMPLX_LOCATION_DATASET is None or SMPLX_FILENAME_DATASET is None:
-    #     print(f'The dataset file does not exit! Ignore checking provided .NPZ file! {SMPLX_TAKE_DATASET}')
-    #     return SMPLX_TAKE
-    
-    # smplx_dataset_data = np.load(SMPLX_TAKE_DATASET, allow_pickle=True)
-    
-    speaker = SMPLX_FILENAME.split("_")[1]
-    speaker_heights = {'ayana': 1.09, 'carla': 1.36, 'carlos': 1.19, 'daiki': 1.34, 'goto': 1.12, 'hailing': 1.21, 'itoi': 1.26, 'jorge': 1.38, 
-                       'katya': 1.12, 'kexin': 1.11, 'kieks': 1.29, 'lawrence': 1.28, 'li': 1.34, 'lu': 1.26, 'luqi': 1.21, 'miranda': 1.21, 'nidal': 1.41, 
-                       'scott': 1.31, 'solomon': 1.45, 'sophie': 1.2, 'stewart': 1.33, 'tiffnay': 1.17, 'wayne': 1.38, 'yingqing': 1.2, 'zhao': 1.34}
+    # speaker = SMPLX_FILENAME.split("_")[1]
+    # speaker_heights = {'ayana': 1.09, 'carla': 1.36, 'carlos': 1.19, 'daiki': 1.34, 'goto': 1.12, 'hailing': 1.21, 'itoi': 1.26, 'jorge': 1.38, 
+    #                    'katya': 1.12, 'kexin': 1.11, 'kieks': 1.29, 'lawrence': 1.28, 'li': 1.34, 'lu': 1.26, 'luqi': 1.21, 'miranda': 1.21, 'nidal': 1.41, 
+    #                    'scott': 1.31, 'solomon': 1.45, 'sophie': 1.2, 'stewart': 1.33, 'tiffnay': 1.17, 'wayne': 1.38, 'yingqing': 1.2, 'zhao': 1.34}
     
     # CORRECT .NPZ
     os.makedirs(str(SMPLX_LOCATION) + '/body_shape', exist_ok=True)
-    # if not os.path.isfile(os.path.join(str(SMPLX_LOCATION) + '/body_shape/', SMPLX_FILENAME)):
     
-    added_gender = 'neutral'
-    print('Gender should always be "neutral": ' + str(added_gender))
-    
-    added_model = 'smplx2020'
-    print('Model: ' + str(added_model))
-    
-    if 'betas' not in smplx_loaded_data:
-        added_betas = np.zeros(300, dtype=int)
-    else:
-        added_betas = smplx_loaded_data['betas']
-    
-    # if not np.allclose(speaker_heights[speaker], smplx_loaded_data['trans'][1][1], rtol=0.2):
-        
-        # min_len = smplx_loaded_data['trans'].shape[0]
-        # if smplx_loaded_data['trans'].shape[0] != smplx_dataset_data['trans'].shape[0]:
-        #     min_len = min(smplx_loaded_data['trans'].shape[0], smplx_dataset_data['trans'].shape[0])
-        #     print('Shape does not match. Taking minimum frames! ' + str(min_len))
-    
-    print('Prev Trans: ' + str(smplx_loaded_data['trans']))
-    temp = smplx_loaded_data['trans'].copy()
-    offset = speaker_heights[speaker] - smplx_loaded_data['trans'][1][1]
-    temp[:, 1] += offset
-    added_trans = temp
-    print('Trans: ' + str(added_trans))
-    
-    if 'expressions' not in smplx_loaded_data:
-        added_expressions = np.zeros((len(smplx_loaded_data['poses']), 100), dtype=float)
-        print('Expressions set to 0')
-    else:
-        added_expressions = smplx_loaded_data['expressions']
-    print('Expressions: ' + str(added_expressions))
-    
-    if 'mocap_frame_rate' not in smplx_loaded_data:
-        added_framrate = 30
-        print('Framerate set to 30, since value is missing')
-    else:
-        added_framrate = smplx_loaded_data['mocap_frame_rate']
-    print('Framerate: ' + str(added_framrate))
-    
-    if 'poses' not in smplx_loaded_data:
-        print('This .NPZ does not contain any animation data!')
-        exit()
-    else:
-        added_poses = smplx_loaded_data['poses']
-    print('Poses: ' + str(added_poses))
-    
+    updated = {**smplx_dataset_data}
+    # The motion is taken from the input file
+    updated['poses'] = smplx_loaded_data['poses']
+    updated['trans'] = smplx_loaded_data['trans']
+    # Facial expressions are zeroed out
+    updated['expressions'] = np.zeros((len(smplx_loaded_data['poses']), 100), dtype=float)
+    # These parameters are constant for the visualiser
+    updated['mocap_frame_rate'] = 30
+    updated['model'] = "smplx2020"
+    updated['gender'] = "neutral"
     np.savez(os.path.join(str(SMPLX_LOCATION) + '/body_shape/', SMPLX_FILENAME),
-                betas=added_betas, 
-                poses=added_poses, 
-                expressions=added_expressions,
-                trans=added_trans,
-                model=added_model,
-                gender=added_gender,
-                mocap_frame_rate=smplx_loaded_data['mocap_frame_rate'])
+             **updated)
     SMPLX_TAKE = myPath(str(SMPLX_LOCATION) + '/body_shape/' + SMPLX_FILENAME + '.npz')
     print(SMPLX_TAKE)
     
